6cea57238eb76ef149316ff0c30bb5c963640dc7
clownmeat <reenyobby@yahoo.com>
2023-05-15 18:06:35 +0000

    capstone project started

---
+++ 7_Week/capstone-project/capstone-project/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  
+  
+  
+  return (
+    <section>
+      <div>
+      <h1 id="page-title">What is a FreeMason</h1>
+      </div>
+      <section>
+      <p></p>
+      </section>
+    </section>
+    
+   
+    
+  
+  
+  
+    )
+
+
+}
+
+export default App;
