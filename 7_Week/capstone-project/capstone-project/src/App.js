181f2a9dea4ea3cb9a1c19ac42a265f255f78984
clownmeat <reenyobby@yahoo.com>
2023-05-15 18:18:15 +0000

    update to index.html

--- 7_Week/capstone-project/capstone-project/src/App.js
+++ 7_Week/capstone-project/capstone-project/src/App.js
@@ -8,17 +8,9 @@ function App() {
     <section>
       <div>
       <h1 id="page-title">What is a FreeMason</h1>
-      </div>
-      <section>
       <p>What do you think a freemason is?</p>
-      </section>
+      </div>
     </section>
-    
-   
-    
-  
-  
-  
     )
 
 
